676bcb6a48e4b55ad62d43e4a4fa4762f61a2b05
WagnerFFreitas <wagnerffreitas1973@gmail.com>
2024-02-10 15:56:36 -0300

    Atualização

--- ADJPA01/index.php
+++ ADJPA01/index.php
@@ -1,22 +1,17 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta name="description" content="Site da Igreja Evangelica Pentecostal de Ouro Verde de Minas">
-    	<meta name=”keywords” content=”iep,iepov,pentecostal,igreja,evangelica,”>
-    	<meta name="author" content="Augusto Ramos">
-    	<link rel="shortcut icon" href="img/iep.png" />
-    	<title>Igreja Evangelica Pentecostal Ouro Verde de Minas</title>
+	<meta name="description" content="Site da Igreja Evangelica Pentecostal de Ouro Verde de Minas">
+    <meta name=”keywords” content=”iep,iepov,pentecostal,igreja,evangelica,”>
+    <meta name="author" content="Augusto Ramos">
+    <link rel="shortcut icon" href="img/iep.png" />
+    <title>Igreja Evangelica Pentecostal Ouro Verde de Minas</title>
 		<frameset rows="5%,*" frameborder="0">
-			<frame src="players/index.php?skin=flash" noresize="no" scrolling="no"></frame>
-			<frame src="home.php"></frame>
+		<frame src="players/index.php?skin=flash" noresize="no" scrolling="no"></frame>
+		<frame src="home.php"></frame>
 		</frameset>
 	</head>
 	<body>
 
 	</body>
 </html>
-<!--
-sem_rtmp
-html5
-flash
--->
